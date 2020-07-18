cb4d3b208a124c52884c57ec6604c18723991cca
GregoryValeryS <GregoryValeryS@gamil.com>
2020-07-18 12:25:31 +0500

    скрипт запросов и обновления базы проверяемых людей дописан

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import json
 from time import sleep
 import pandas
 
-global URL, ACCESS_TOKEN
+global URL, ACCESS_TOKEN, FILE, TIME_FOR_ANSWER
 URL = 'https://api-ip.fssprus.ru/api/v1.0/'  # на случай если сервер API изменится
 TOKEN = open(
     'C:/Google Drive/program/matirials_for_fssp_api/fssp_token.txt').read()  # здесь вы указываете свой токен доступа
+FILE = 'C:/Google Drive/program/matirials_for_fssp_api/test.xlsx'  # файл, с которым будете работать
+TIME_FOR_ANSWER = 42  # иногда сервера fssp требуют на обработку групповго запрса минут 5 (в выходыне особенно)
 
 
 def status(task: str):
@@ -14,7 +16,7 @@ def status(task: str):
 
 
 def test():
-    request = requests.get(f'{URL}search/physical', params={
+    """request = requests.get(f'{URL}search/physical', params={
         'token': TOKEN,
         'region': 56,
         'firstname': 'Сергей',
@@ -27,13 +29,14 @@ def test():
     print(request)
     answer = requests.get(f'{URL}result', params={"token": TOKEN, "task": request["response"]["task"]}).json()
 
-    print(answer)
+    print(answer)"""
+    pass
 
 
 def main():
     # запишем данные из экселя и преобразуем их в список словарей, где каждый словарь - человек
-    excel_data_dict = pandas.read_excel(f'C:/Google Drive/program/matirials_for_fssp_api/test.xlsx').to_dict(
-        orient='record')
+
+    excel_data_dict = pandas.read_excel(f'{FILE}').to_dict(orient='record')
     # преобразуем формат даты в текст, удалим лишние пробелы, заменим 'nan' на пустые строки
     person_keys_list = list(excel_data_dict[0].keys())
 
@@ -57,7 +60,6 @@ def main():
     max_single_requests_per_hour = 100  # Максимальное число одиночных запросов в час
     max_single_requests_per_day = 1000  # Максимальное число одиночных запросов в сутки
 
-
     request_counter = 0  # счётчик запросов
 
     group_request_params = {"token": TOKEN, "request": []}  # сформируем параметры группового запроса
@@ -66,25 +68,47 @@ def main():
 
     for region in person_keys_list[4:]:  # заполнять таблицу мы будем регион за регионом для каждого согласно списка
         for person_i in range(0, len(excel_data_dict)):  # перебираем людей для этого региона
-            # если мы уже набрали 50 подзапросов, можно производить запрос
-            if len(subqueries_list) == 10:
+            if excel_data_dict[person_i][region] != 'нет' and len(excel_data_dict[person_i][region]) < 19:
+                # person_i - номер человека в словаре
+                # region - регион в который будет запись
+                subqueries_list.append([person_i, region])
+                print('Групповой запрос [id человека, регион]:', subqueries_list)
+                group_request_params["request"].append(
+                    {
+                        "type": 1,
+                        "params": {
+                            "firstname": excel_data_dict[person_i]['Имя'],
+                            "lastname": excel_data_dict[person_i]['Фамилия'],
+                            "secondname": excel_data_dict[person_i]['Отчество'],
+                            "region": region,
+                            "birthdate": excel_data_dict[person_i]['Дата рождения']
+                        }
+                    }
+                )
+
+            # если мы уже набрали 50 подзапросов или мы в конце списка, можно производить запрос
+            print(region, int(person_keys_list[-1]))
+            print(person_i, len(excel_data_dict) - 1)
+            if len(subqueries_list) == max_subqueries_in_group_request or \
+                    (region == int(person_keys_list[-1]) and person_i == (len(excel_data_dict) - 1)):
                 print('произведём запрос')
                 request_counter += 1
                 sleep(0.34)
                 group_request = requests.post(f'{URL}search/group', json=group_request_params).json()
 
                 print('\nЗапрос', group_request)
-
+                # '' id задачи
                 group_request_task_id = group_request["response"]["task"]
                 group_request_params["request"] = []
 
-
                 while True:
                     request_counter += 1
-                    sleep(4.2)  # кто знает, сколько серверу понадобится для обработки запроса?
+                    print(f'Дадим время на обратку запроса - {TIME_FOR_ANSWER} сек.')
+                    sleep(TIME_FOR_ANSWER)  # иногда - минута, иногда несколько секунд
                     answer = requests.get(f'{URL}result', params={"token": TOKEN, "task": group_request_task_id}).json()
                     print('Задача ещё исполняется')
                     if answer["response"]["task_end"] is not None:
+                        print('Задача выполнена')
                         break
 
                 print('\nОтвет', answer)
@@ -106,31 +130,17 @@ def main():
                     # запишем полученный ответ(ы)
                     # writing_data subqueries_list[number_of_person][0] - номер человека в словаре
                     # [subqueries_list[number_of_person][1]] - регион в который будет запись
-                    excel_data_dict[subqueries_list[number_of_person][0]][subqueries_list[number_of_person][1]] = writing_data
+                    excel_data_dict[subqueries_list[number_of_person][0]][
+                        subqueries_list[number_of_person][1]] = writing_data
                     number_of_person += 1
+
                 subqueries_list = []
-                print(excel_data_dict)
-                print('')
+                # преобразовываем обратно в data frame
+                data_frame = pandas.DataFrame.from_dict(excel_data_dict)
+                # перезаписываем файл в excel
+                data_frame.to_excel(f'{FILE}', index=False)
 
-            if excel_data_dict[person_i][region] != 'нет':
-                # person_i - номер человека в словаре
-                # region - регион в который будет запись
-                subqueries_list.append([person_i, region])
-                print(subqueries_list)
-                group_request_params["request"].append(
-                    {
-                        "type": 1,
-                        "params": {
-                            "firstname": excel_data_dict[person_i]['Имя'],
-                            "lastname": excel_data_dict[person_i]['Фамилия'],
-                            "secondname": excel_data_dict[person_i]['Отчество'],
-                            "region": region,
-                            "birthdate": excel_data_dict[person_i]['Дата рождения']
-                        }
-                    }
-                )
-
-
+    print('Работа с файлом завершена')
 
 
 if __name__ == '__main__':
